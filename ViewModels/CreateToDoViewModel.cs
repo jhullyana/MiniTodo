19a26cd695e396ea95fd2cf74b04e2ffeef1ff01
jhullyana <jhullyana01@gmail.com>
2024-06-24 19:39:20 -0300

    Arrumando bugs

--- ViewModels/CreateToDoViewModel.cs
+++ ViewModels/CreateToDoViewModel.cs
@@ -1,31 +1,23 @@
-using System;
-
+using System.Diagnostics.Contracts;
 using Flunt.Notifications;
+using Flunt.Validations;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace MiniTodo.ViewModels
 {
     public class CreateToDoViewModel : Notifiable<Notification>
     {
         public string Title { get; set; }
-
-        public ToDo MapTo()
+        
+   
+      public ToDo MapTo()
         {
-            var contract = new Contract()
-                .Requires()
-                .IsNotNull(Title, "Informe o título da tarefa")
-                .IsGreaterThan(Title, 5, "O título deve conter mais de 5 caracteres");
-
-            AddNotifications(contract);
-
+            AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsNotNull(Title, "Informe o título da tarefa")
+            .IsGreaterThan(Title, 5,"O título deve conter mais de cinco caracteres"));
+            
             return new ToDo(Guid.NewGuid(), Title, false);
-        }
-    }
-
-    public class ToDo
-    {
-        public ToDo(Guid id, string title, bool completed)
-        {
-            // Implementação da classe ToDo
         }
     }
 }
